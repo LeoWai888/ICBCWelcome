f35c9519b3addf7523db46b76890cfbd5c235e89
hardrockguhz <ghzemail@sina.com>
2019-01-25 10:20:29 +0800

    44

--- app/src/main/java/com/icbc/icbcwelcome/Activity/MainActivity.java
+++ app/src/main/java/com/icbc/icbcwelcome/Activity/MainActivity.java
@@ -110,7 +110,6 @@ public class MainActivity extends BaseActivity implements HomeContract.View {
                           public void run() {
                               banner.update(bannnerImgList);
                           }
-                      }
-            );
+                      });
     }
 }
